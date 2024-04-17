0c735e33446ea91cf9a4fcbac8503c0ac805e989
Nicholas Costa <nicholascostadev@gmail.com>
2024-04-16 21:17:11 -0300

    feat: block auth form routes when authenticated

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,11 +1,22 @@
-import { SubmitButton } from './submit-button';
 import { AuthForm } from '@/components/auth-form';
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
 
-export default function SignIn({
+export default async function SignIn({
   searchParams,
 }: {
   searchParams: { message: string };
 }) {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    redirect('/upload');
+  }
+
   return (
     <div className="flex w-full justify-center items-center h-[calc(100vh-64px)]">
       <AuthForm intent="sign-in" message={searchParams.message} />
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,11 +1,22 @@
-import { SubmitButton } from './submit-button';
 import { AuthForm } from '@/components/auth-form';
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
 
-export default function SignUp({
+export default async function SignUp({
   searchParams,
 }: {
   searchParams: { message: string };
 }) {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    redirect('/upload');
+  }
+
   return (
     <div className="flex w-full justify-center items-center h-[calc(100vh-64px)]">
       <AuthForm intent="sign-up" message={searchParams.message} />
