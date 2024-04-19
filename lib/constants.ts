893e27909f45b0646a86abd0b6d9a85c57bb5c7f
Nicholas Costa <nicholascostadev@gmail.com>
2024-04-19 15:04:42 -0300

    refactor: change variable with prefix NEXT_PUBLIC

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_URL = process.env.NEXT_PUBLIC_BASE_URL as string;
 export const MAX_LOGGED_SIZE_BYTES = 1024 * 1024 * 50;
 export const MAX_ANON_SIZE_BYTES = 1024 * 1024 * 30;
-export const OG_IMAGE_URL = new URL(process.env.OG_IMAGE_URL!);
+export const OG_IMAGE_URL = new URL(process.env.NEXT_PUBLIC_OG_IMAGE_URL!);
