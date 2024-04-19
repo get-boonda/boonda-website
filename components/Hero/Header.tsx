72922d6ed55a469eea80137a86244a11266512be
Nicholas Costa <nicholascostadev@gmail.com>
2024-04-19 15:38:20 -0300

    feat: add the text shadow back, but animated

--- components/Hero/Header.tsx
+++ components/Hero/Header.tsx
@@ -1,10 +1,10 @@
 'use client';
 import { ShareNowButton } from './share-now-button';
-import { motion } from 'framer-motion';
+import { motion, useAnimate } from 'framer-motion';
 
 const containerVariants = {
   hidden: {
-    color: '#ffffff',
+    color: 'transparent',
     transition: {
       duration: 2,
     },
@@ -33,43 +33,77 @@ const wordVariants = {
 };
 
 export default function Header() {
+  const [scope, animate] = useAnimate();
+
   return (
     <div className="flex flex-col gap-16 items-center">
       <section className="py-8 pt-24">
         <h1 className="sr-only">Share your files with ease</h1>
 
-        <motion.p
-          variants={containerVariants}
-          initial="hidden"
-          animate="shown"
-          onEnded={() => {}}
-          style={{
-            background:
-              'linear-gradient(to left bottom, rgb(255, 255, 255) 50%, rgba(255, 255, 255, 0.38)) text',
-          }}
-          className="text-5xl lg:text-6xl !leading-tight mx-auto max-w-xl text-center"
-        >
-          <motion.span variants={wordVariants} className="inline-block">
-            Where{' '}
-          </motion.span>
-          <motion.span
-            variants={wordVariants}
-            className="font-bold inline-block pl-2.5"
+        <div className="relative">
+          <span className="sr-only">Where sharing meets simplicity</span>
+          <motion.p
+            aria-hidden="true"
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 1,
+            }}
+            transition={{
+              delay: 2.2,
+            }}
+            style={{
+              background:
+                'linear-gradient(to left bottom, rgb(255, 255, 255) 50%, rgba(255, 255, 255, 0.38)) text',
+            }}
+            className="text-5xl z-[2] lg:text-6xl text-transparent !leading-tight mx-auto max-w-xl text-center absolute"
           >
-            {' '}
-            sharing
-          </motion.span>{' '}
-          <motion.span variants={wordVariants} className="inline-block">
-            meets
-          </motion.span>
-          <motion.span
-            variants={wordVariants}
-            className="font-bold inline-block pl-2.5"
+            <span className="inline-block">Where </span>
+            <span className="font-bold inline-block pl-2.5"> sharing</span>
+            <span className="inline-block max-[513px]:pl-0 max-[1025px]:pl-2.5">
+              meets
+            </span>
+            <span className="font-bold inline-block pl-2.5">simplicity</span>
+          </motion.p>
+          <motion.p
+            aria-hidden="true"
+            ref={scope}
+            variants={containerVariants}
+            initial="hidden"
+            animate="shown"
+            onEnded={() => {}}
+            style={{
+              background:
+                'linear-gradient(to left bottom, rgb(255, 255, 255) 50%, rgba(255, 255, 255, 0.38)) text',
+            }}
+            className="text-5xl select-none lg:text-6xl !leading-tight mx-auto max-w-xl text-center"
           >
-            {' '}
-            simplicity
-          </motion.span>
-        </motion.p>
+            <motion.span variants={wordVariants} className="inline-block">
+              Where{' '}
+            </motion.span>
+            <motion.span
+              variants={wordVariants}
+              className="font-bold inline-block pl-2.5"
+            >
+              {' '}
+              sharing
+            </motion.span>{' '}
+            <motion.span variants={wordVariants} className="inline-block">
+              meets
+            </motion.span>
+            <motion.span
+              variants={wordVariants}
+              className="font-bold inline-block pl-2.5"
+              onAnimationComplete={() => {
+                animate(scope.current, { opacity: 0 });
+              }}
+            >
+              {' '}
+              simplicity
+            </motion.span>
+          </motion.p>
+        </div>
 
         <motion.div
           className="flex justify-center items-center pt-12"
