3ff3fd466c5f0c1395d8e5d18db05d2d51a369ff
Thalles Passos <contato@thalles.me>
2024-04-19 23:21:11 -0300

    feat: add download page

---
+++ app/(common)/download/page.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { FaApple, FaWindows, FaLinux } from "react-icons/fa";
+import { motion, useAnimate } from "framer-motion";
+import Link from "next/link";
+
+export default function DownloadPage() {
+  const [scope, animate] = useAnimate();
+
+  function handleHover() {
+    animate(scope.current, { filter: "blur(46px)", inset: -4 });
+  }
+
+  function handleRemoveHover() {
+    animate(scope.current, { filter: "blur(32px)", inter: 0 });
+  }
+
+  return (
+    <div className="flex-1 w-full flex flex-col items-center">
+      <div className="py-24">
+        <h1 className="text-5xl">
+          Get <span className="font-bold inline-block">Boonda</span> on your
+          platform
+        </h1>
+      </div>
+      <div className="flex gap-3">
+        <Button className="flex justify-center gap-1" size="lg" asChild>
+          <Link
+            href="https://dagrxagqkexvgylcbiqz.supabase.co/storage/v1/object/public/opengraph/paper-1.19.4-550.jar?t=2024-04-20T01%3A35%3A01.325Z"
+            download
+          >
+            <FaApple size={24} /> MacOS
+          </Link>
+        </Button>
+        <Button className="flex gap-3" size="lg">
+          <FaWindows size={24} />
+          Windows
+        </Button>
+        <Button className="flex gap-3" size="lg">
+          <FaLinux size={24} />
+          Linux
+        </Button>
+      </div>
+      <div className="w-full container relative pt-24 group">
+        <div
+          style={{
+            background:
+              "conic-gradient(from 230.29deg at 51.63% 52.16%, rgb(36, 0, 255) 0deg, rgb(0, 135, 255) 67.5deg, rgb(108, 39, 157) 198.75deg, rgb(24, 38, 163) 251.25deg, rgb(54, 103, 196) 301.88deg, rgb(105, 30, 255) 360deg)",
+          }}
+          className="blur-[160px] opacity-35 w-full h-full absolute inset-0 top-12 pointer-events-none"
+        />
+        <div
+          onMouseOver={handleHover}
+          onMouseLeave={handleRemoveHover}
+          className="rounded-lg relative"
+        >
+          <motion.div
+            ref={scope}
+            initial={{
+              filter: "blur(28px)",
+              opacity: 0.8,
+              inset: 0,
+            }}
+            animate={{
+              filter: "blur(36px)",
+              opacity: 1,
+              repeatCount: Infinity,
+            }}
+            transition={{
+              duration: 2,
+              ease: "backOut",
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+            className="absolute bg-purple-600/50 blur-xl transition-all rounded-lg opacity-50"
+          ></motion.div>
+          <motion.video
+            initial="shown"
+            src="https://dagrxagqkexvgylcbiqz.supabase.co/storage/v1/object/public/opengraph/desktop_video.mp4"
+            width={1370}
+            height={776}
+            autoPlay
+            muted
+            loop
+            className="border z-[1] bg-zinc-950 relative border-border rounded-lg mx-auto"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/Hero/Header.tsx
+++ components/Hero/Header.tsx
@@ -1,16 +1,18 @@
-'use client';
-import { ShareNowButton } from './share-now-button';
-import { motion } from 'framer-motion';
+"use client";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { ShareNowButton } from "./share-now-button";
+import { motion } from "framer-motion";
 
 const containerVariants = {
   hidden: {
-    color: '#ffffff',
+    color: "#ffffff",
     transition: {
       duration: 2,
     },
   },
   shown: {
-    color: 'unset',
+    color: "unset",
     transition: {
       staggerChildren: 0.25,
     },
@@ -27,7 +29,7 @@ const wordVariants = {
     y: 0,
     transition: {
       duration: 1.5,
-      ease: 'easeOut',
+      ease: "easeOut",
     },
   },
 };
@@ -36,7 +38,7 @@ export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
       <section className="py-8 pt-24">
-        <h1 className="sr-only">Share your files with ease</h1>
+        <h1 className="sr-only">Where sharing meets simplicity</h1>
 
         <motion.p
           variants={containerVariants}
@@ -45,20 +47,20 @@ export default function Header() {
           onEnded={() => {}}
           style={{
             background:
-              'linear-gradient(to left bottom, rgb(255, 255, 255) 50%, rgba(255, 255, 255, 0.38)) text',
+              "linear-gradient(to left bottom, rgb(255, 255, 255) 50%, rgba(255, 255, 255, 0.38)) text",
           }}
           className="text-5xl lg:text-6xl !leading-tight mx-auto max-w-xl text-center"
         >
           <motion.span variants={wordVariants} className="inline-block">
-            Where{' '}
+            Where{" "}
           </motion.span>
           <motion.span
             variants={wordVariants}
             className="font-bold inline-block pl-2.5"
           >
-            {' '}
+            {" "}
             sharing
-          </motion.span>{' '}
+          </motion.span>{" "}
           <motion.span variants={wordVariants} className="inline-block">
             meets
           </motion.span>
@@ -66,13 +68,13 @@ export default function Header() {
             variants={wordVariants}
             className="font-bold inline-block pl-2.5"
           >
-            {' '}
+            {" "}
             simplicity
           </motion.span>
         </motion.p>
 
         <motion.div
-          className="flex justify-center items-center pt-12"
+          className="flex flex-col justify-center items-center pt-12"
           initial={{ opacity: 0, y: 50 }}
           animate={{
             opacity: 1,
@@ -81,6 +83,12 @@ export default function Header() {
           transition={{ delay: 2.5, duration: 0.5 }}
         >
           <ShareNowButton />
+          <div className="pt-2">
+            <span className="text-sm">or download our </span>
+            <Button className="p-0 h-auto" variant="link" asChild>
+              <Link href="/download">desktop app</Link>
+            </Button>
+          </div>
         </motion.div>
       </section>
     </div>
