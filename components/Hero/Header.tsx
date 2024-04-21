04f15245e361ba0f1c92f77ce5a5621c8468bf53
Thalles Passos <contato@thalles.me>
2024-04-21 01:11:02 -0300

    fix: 500 on main page

--- components/Hero/Header.tsx
+++ components/Hero/Header.tsx
@@ -1,4 +1,6 @@
 "use client";
+import Link from "next/link";
+import { Button } from "../ui/button";
 import { ShareNowButton } from "./share-now-button";
 import { motion, useAnimate } from "framer-motion";
 
